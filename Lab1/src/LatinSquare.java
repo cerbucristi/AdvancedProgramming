1836f41583bd2b58f8fd345c8a89629fab41853d
cerbucristi <61745916+cerbucristi@users.noreply.github.com>
2023-02-26 15:15:44 +0200

    An implementation for the Latin Square problem
    Implemented homework

---
+++ Lab1/src/LatinSquare.java
@@ -0,0 +1,37 @@
+public class LatinSquare {
+
+    private int n;
+    private int[][] matrixLatinSquare;
+
+
+    public LatinSquare(int n) {
+        this.n = n;
+        matrixLatinSquare = initializeMatrix(n);
+    }
+
+    private int[][] initializeMatrix(int n) {
+        int[][] retMatrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++){
+                retMatrix[i][j] = (j+i) % n + 1;
+            }
+        }
+        return retMatrix;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int[][] getMatrixLatinSquare() {
+        return matrixLatinSquare;
+    }
+
+    public void setMatrixLatinSquare(int[][] matrixLatinSquare) {
+        this.matrixLatinSquare = matrixLatinSquare;
+    }
+}
